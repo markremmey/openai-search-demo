14854198012d2b5abcd18d981baf9e60ca65b19c
markremmey <53156818+markremmey@users.noreply.github.com>
2023-06-01 18:20:22 -0400

    Update app.py

--- app/backend/app.py
+++ app/backend/app.py
@@ -33,7 +33,7 @@ azure_credential = DefaultAzureCredential()
 
 # Used by the OpenAI SDK
 openai.api_type = "azure"
-openai.api_base = f"https://{AZURE_OPENAI_SERVICE}.openai.azure.com"
+openai.api_base = f"https://{AZURE_OPENAI_SERVICE}.openai.azure.com/openai"
 openai.api_version = "2022-12-01"
 
 # Comment these two lines out if using keys, set your API key in the OPENAI_API_KEY environment variable instead
